2697b3482aee5e78c6615f1904011e9cd0f7d06d
NazmulHasanNahin <nahinn75@gmail.com>
2025-04-11 22:12:43 +0600

    crud

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.port || 5000;
@@ -30,9 +30,57 @@ async function run() {
 
         // code start here 
 
-
         const alluser = client.db("userDB").collection("users")
 
+        // to get all data via get 
+        app.get("/users", async (req, res) => {
+            const cursor = alluser.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        //update 
+
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await alluser.findOne(query);
+            res.send(result)
+        })
+
+
+        app.post("/users", async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await alluser.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.put("/users/:id",async(req,res)=>{
+            const id = req.params.id;
+            const filter = {_id : new ObjectId(id)};
+            const option = {upsert : true };
+            const user = req.body
+            const updateduser = {
+                $set :{
+                    name:user.name,
+                    age:user.age,
+                    yob:user.yob,
+                    fathername:user.fathername,
+                    name:user.mothername,
+
+                }
+            };
+            const result = await alluser.updateOne(filter,updateduser,option)
+            res.send(result);
+        })
+
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await alluser.deleteOne(query);
+            res.send(result);
+        })
 
 
 
